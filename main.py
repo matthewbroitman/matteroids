c9fdc9a629ad883ca20dfb67714eab5bcd42788d
matthewbroitman <matthewbroitman@gmail.com>
2025-09-07 23:55:37 -0300

    B: Player added and defined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,13 +8,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    FPS=pygame.time.Clock()
+    dt=0
+    x=SCREEN_WIDTH/2
+    y=SCREEN_HEIGHT/2
+    player1 = Player(x,y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill ("black")
+           
+        screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+        ms=FPS.tick(60)
+        dt = ms / 1000
+
+        
+        
     
     
 
